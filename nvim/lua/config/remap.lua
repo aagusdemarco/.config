001685ca8d60d924ac44474e907e88bfbb61a424
Agustin Tomas Demarco <aagusdemarco@proton.me>
2025-08-19 20:53:07 -0300

    eliminar configs ajenas a linux

--- nvim/lua/config/remap.lua
+++
@@ -1,8 +0,0 @@
--- Asignar leader a espacio
-vim.g.mapleader = ' '
-
--- Volver a netrw
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
-
--- Formatear documento con LSP
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
